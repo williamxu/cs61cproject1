84c565239c809d240014a0b3757c5c06793a9295
William Xu <xu.will94@gmail.com>
2013-09-21 18:02:18 -0700

    wrote up map and reduce part 1

--- Proj1.java
+++ Proj1.java
@@ -22,236 +22,290 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 /*
  * This is the skeleton for CS61c project 1, Fall 2013.
- *
  * Reminder:  DO NOT SHARE CODE OR ALLOW ANOTHER STUDENT TO READ YOURS.
+ *
  * EVEN FOR DEBUGGING. THIS MEANS YOU.
  *
  */
-public class Proj1{
+public class Proj1 {
 
-    /*
-     * Inputs is a set of (docID, document contents) pairs.
-     */
-    public static class Map1 extends Mapper<WritableComparable, Text, Text, Text> {
-        /** Regex pattern to find words (alphanumeric + _). */
-        final static Pattern WORD_PATTERN = Pattern.compile("\\w+");
+	/*
+	 * Inputs is a set of (docID, document contents) pairs.
+	 */
+	public static class Map1 extends
+			Mapper<WritableComparable, Text, Text, Text> {
+		/** Regex pattern to find words (alphanumeric + _). */
+		final static Pattern WORD_PATTERN = Pattern.compile("\\w+");
 
-        private String targetGram = null;
-        private int funcNum = 0;
+		private String targetGram = null;
+		private int funcNum = 0;
 
-        /*
-         * Setup gets called exactly once for each mapper, before map() gets called the first time.
-         * It's a good place to do configuration or setup that can be shared across many calls to map
-         */
-        @Override
-            public void setup(Context context) {
-                targetGram = context.getConfiguration().get("targetWord").toLowerCase();
-                try {
-                    funcNum = Integer.parseInt(context.getConfiguration().get("funcNum"));
-                } catch (NumberFormatException e) {
-                    /* Do nothing. */
-                }
-            }
+		/*
+		 * Setup gets called exactly once for each mapper, before map() gets
+		 * called the first time. It's a good place to do configuration or setup
+		 * that can be shared across many calls to map
+		 */
+		@Override
+		public void setup(Context context) {
+			targetGram = context.getConfiguration().get("targetWord")
+					.toLowerCase();
+			try {
+				funcNum = Integer.parseInt(context.getConfiguration().get(
+						"funcNum"));
+			} catch (NumberFormatException e) {
+				/* Do nothing. */
+			}
+		}
 
-        @Override
-            public void map(WritableComparable docID, Text docContents, Context context)
-            throws IOException, InterruptedException {
-                Matcher matcher = WORD_PATTERN.matcher(docContents.toString());
-                Func func = funcFromNum(funcNum);
+		@Override
+		public void map(WritableComparable docID, Text docContents,
+				Context context) throws IOException, InterruptedException {
+			Matcher matcher = WORD_PATTERN.matcher(docContents.toString());
+			Func func = funcFromNum(funcNum);
 
-                // YOUR CODE HERE
+			// YOUR CODE HERE
+			ArrayList<String> words = new ArrayList<String>();
+			while (matcher.find()) {
+				String s = matcher.group().toLowerCase();
+				words.add(s);
+				}
+			double d = 0.0;
+			for (int i = 0; i < words.size(); i++) {
+				String current = words.get(i);
+				if (current != targetGram) {
+					int before = words.subList(0, i).lastIndexOf(targetGram);
+					int after = words.subList(i+1, words.size()).indexOf(targetGram);
+					if (before == -1 && after == -1){
+						d = Double.POSITIVE_INFINITY;
+					}
+					else if (before == -1) {
+						d = after - i;
+					}
+					else if (after == -1){
+						d = i - before;
+					}
+					else {
+						d = Math.min(i-before, after-i);
+					}
+					context.write(new Text(current), new DoublePair(1.0, func.f(d)));
+				}
+			}
+		
+		}
 
-            }
+		/** Returns the Func corresponding to FUNCNUM */
+		private Func funcFromNum(int funcNum) {
+			Func func = null;
+			switch (funcNum) {
+			case 0:
+				func = new Func() {
+					public double f(double d) {
+						return d == Double.POSITIVE_INFINITY ? 0.0 : 1.0;
+					}
+				};
+				break;
+			case 1:
+				func = new Func() {
+					public double f(double d) {
+						return d == Double.POSITIVE_INFINITY ? 0.0
+								: 1.0 + 1.0 / d;
+					}
+				};
+				break;
+			case 2:
+				func = new Func() {
+					public double f(double d) {
+						return d == Double.POSITIVE_INFINITY ? 0.0 : 1.0 + Math
+								.sqrt(d);
+					}
+				};
+				break;
+			}
+			return func;
+		}
+	}
 
-        /** Returns the Func corresponding to FUNCNUM*/
-        private Func funcFromNum(int funcNum) {
-            Func func = null;
-            switch (funcNum) {
-                case 0:	
-                    func = new Func() {
-                        public double f(double d) {
-                            return d == Double.POSITIVE_INFINITY ? 0.0 : 1.0;
-                        }			
-                    };	
-                    break;
-                case 1:
-                    func = new Func() {
-                        public double f(double d) {
-                            return d == Double.POSITIVE_INFINITY ? 0.0 : 1.0 + 1.0 / d;
-                        }			
-                    };
-                    break;
-                case 2:
-                    func = new Func() {
-                        public double f(double d) {
-                            return d == Double.POSITIVE_INFINITY ? 0.0 : 1.0 + Math.sqrt(d);
-                        }			
-                    };
-                    break;
-            }
-            return func;
-        }
-    }
+	/**
+	 * Here's where you'll be implementing your combiner. It must be non-trivial
+	 * for you to receive credit.
+	 */
+	public static class Combine1 extends Reducer<Text, DoublePair, Text, DoublePair> {
 
-    /** Here's where you'll be implementing your combiner. It must be non-trivial for you to receive credit. */
-    public static class Combine1 extends Reducer<Text, Text, Text, Text> {
+		@Override
+		public void reduce(Text key, Iterable<DoublePair> values, Context context)
+				throws IOException, InterruptedException {
 
-        @Override
-            public void reduce(Text key, Iterable<Text> values,
-                    Context context) throws IOException, InterruptedException {
+			double count = 0;
+			double sum = 0;
+			// YOUR CODE HERE
+			for (DoublePair d : values) {
+				count += d.getDouble1();
+				sum += d.getDouble2();
+			}
+			context.write(key, new DoublePair(count, sum));
+		}
+	}
 
-                 // YOUR CODE HERE
+	public static class Reduce1 extends
+			Reducer<Text, DoublePair, DoubleWritable, Text> {
+		@Override
+		public void reduce(Text key, Iterable<DoublePair> values, Context context)
+				throws IOException, InterruptedException {
 
-            }
-    }
+			// YOUR CODE HERE
+			double count = 0;
+			double sum = 0;
+			// YOUR CODE HERE
+			for (DoublePair d : values) {
+				count += d.getDouble1();
+				sum += d.getDouble2();
+			}
+			if (sum == 0) context.write(new DoubleWritable(0), key);
+			else context.write(new DoubleWritable(sum * Math.pow(Math.log(sum),3)/count));
+		}
+	}
 
+	public static class Map2 extends
+			Mapper<DoubleWritable, Text, DoubleWritable, Text> {
+		// maybe do something, maybe don't
+	}
 
-    public static class Reduce1 extends Reducer<Text, Text, DoubleWritable, Text> {
-        @Override
-            public void reduce(Text key, Iterable<Text> values,
-                    Context context) throws IOException, InterruptedException {
+	public static class Reduce2 extends
+			Reducer<DoubleWritable, Text, DoubleWritable, Text> {
 
-                // YOUR CODE HERE
+		int n = 0;
+		static int N_TO_OUTPUT = 100;
 
-            }
-    }
+		/*
+		 * Setup gets called exactly once for each reducer, before reduce() gets
+		 * called the first time. It's a good place to do configuration or setup
+		 * that can be shared across many calls to reduce
+		 */
+		@Override
+		protected void setup(Context c) {
+			n = 0;
+		}
 
-    public static class Map2 extends Mapper<DoubleWritable, Text, DoubleWritable, Text> {
-        //maybe do something, maybe don't
-    }
+		/*
+		 * Your output should be a in the form of (DoubleWritable score, Text
+		 * word) where score is the co-occurrence value for the word. Your
+		 * output should be sorted from largest co-occurrence to smallest
+		 * co-occurrence.
+		 */
+		@Override
+		public void reduce(DoubleWritable key, Iterable<Text> values,
+				Context context) throws IOException, InterruptedException {
 
-    public static class Reduce2 extends Reducer<DoubleWritable, Text, DoubleWritable, Text> {
+			// YOUR CODE HERE
 
-        int n = 0;
-        static int N_TO_OUTPUT = 100;
+		}
+	}
 
-        /*
-         * Setup gets called exactly once for each reducer, before reduce() gets called the first time.
-         * It's a good place to do configuration or setup that can be shared across many calls to reduce
-         */
-        @Override
-            protected void setup(Context c) {
-                n = 0;
-            }
+	/*
+	 * You shouldn't need to modify this function much. If you think you have a
+	 * good reason to, you might want to discuss with staff.
+	 * 
+	 * The skeleton supports several options. if you set runJob2 to false, only
+	 * the first job will run and output will be in TextFile format, instead of
+	 * SequenceFile. This is intended as a debugging aid.
+	 * 
+	 * If you set combiner to false, the combiner will not run. This is also
+	 * intended as a debugging aid. Turning on and off the combiner shouldn't
+	 * alter your results. Since the framework doesn't make promises about when
+	 * it'll invoke combiners, it's an error to assume anything about how many
+	 * times values will be combined.
+	 */
+	public static void main(String[] rawArgs) throws Exception {
+		GenericOptionsParser parser = new GenericOptionsParser(rawArgs);
+		Configuration conf = parser.getConfiguration();
+		String[] args = parser.getRemainingArgs();
 
-        /*
-         * Your output should be a in the form of (DoubleWritable score, Text word)
-         * where score is the co-occurrence value for the word. Your output should be
-         * sorted from largest co-occurrence to smallest co-occurrence.
-         */
-        @Override
-            public void reduce(DoubleWritable key, Iterable<Text> values,
-                    Context context) throws IOException, InterruptedException {
+		boolean runJob2 = conf.getBoolean("runJob2", true);
+		boolean combiner = conf.getBoolean("combiner", false);
 
-                 // YOUR CODE HERE
+		System.out.println("Target word: " + conf.get("targetWord"));
+		System.out.println("Function num: " + conf.get("funcNum"));
 
-            }
-    }
+		if (runJob2)
+			System.out.println("running both jobs");
+		else
+			System.out.println("for debugging, only running job 1");
 
-    /*
-     *  You shouldn't need to modify this function much. If you think you have a good reason to,
-     *  you might want to discuss with staff.
-     *
-     *  The skeleton supports several options.
-     *  if you set runJob2 to false, only the first job will run and output will be
-     *  in TextFile format, instead of SequenceFile. This is intended as a debugging aid.
-     *
-     *  If you set combiner to false, the combiner will not run. This is also
-     *  intended as a debugging aid. Turning on and off the combiner shouldn't alter
-     *  your results. Since the framework doesn't make promises about when it'll
-     *  invoke combiners, it's an error to assume anything about how many times
-     *  values will be combined.
-     */
-    public static void main(String[] rawArgs) throws Exception {
-        GenericOptionsParser parser = new GenericOptionsParser(rawArgs);
-        Configuration conf = parser.getConfiguration();
-        String[] args = parser.getRemainingArgs();
+		if (combiner)
+			System.out.println("using combiner");
+		else
+			System.out.println("NOT using combiner");
 
-        boolean runJob2 = conf.getBoolean("runJob2", true);
-        boolean combiner = conf.getBoolean("combiner", false);
+		Path inputPath = new Path(args[0]);
+		Path middleOut = new Path(args[1]);
+		Path finalOut = new Path(args[2]);
+		FileSystem hdfs = middleOut.getFileSystem(conf);
+		int reduceCount = conf.getInt("reduces", 32);
 
-        System.out.println("Target word: " + conf.get("targetWord"));
-        System.out.println("Function num: " + conf.get("funcNum"));
+		if (hdfs.exists(middleOut)) {
+			System.err.println("can't run: " + middleOut.toUri().toString()
+					+ " already exists");
+			System.exit(1);
+		}
+		if (finalOut.getFileSystem(conf).exists(finalOut)) {
+			System.err.println("can't run: " + finalOut.toUri().toString()
+					+ " already exists");
+			System.exit(1);
+		}
 
-        if(runJob2)
-            System.out.println("running both jobs");
-        else
-            System.out.println("for debugging, only running job 1");
+		{
+			Job firstJob = new Job(conf, "job1");
 
-        if(combiner)
-            System.out.println("using combiner");
-        else
-            System.out.println("NOT using combiner");
+			firstJob.setJarByClass(Map1.class);
 
-        Path inputPath = new Path(args[0]);
-        Path middleOut = new Path(args[1]);
-        Path finalOut = new Path(args[2]);
-        FileSystem hdfs = middleOut.getFileSystem(conf);
-        int reduceCount = conf.getInt("reduces", 32);
+			/* You may need to change things here */
+			firstJob.setMapOutputKeyClass(Text.class);
+			firstJob.setMapOutputValueClass(DoublePair.class);
+			firstJob.setOutputKeyClass(DoubleWritable.class);
+			firstJob.setOutputValueClass(Text.class);
+			/* End region where we expect you to perhaps need to change things. */
 
-        if(hdfs.exists(middleOut)) {
-            System.err.println("can't run: " + middleOut.toUri().toString() + " already exists");
-            System.exit(1);
-        }
-        if(finalOut.getFileSystem(conf).exists(finalOut) ) {
-            System.err.println("can't run: " + finalOut.toUri().toString() + " already exists");
-            System.exit(1);
-        }
+			firstJob.setMapperClass(Map1.class);
+			firstJob.setReducerClass(Reduce1.class);
+			firstJob.setNumReduceTasks(reduceCount);
 
-        {
-            Job firstJob = new Job(conf, "job1");
+			if (combiner)
+				firstJob.setCombinerClass(Combine1.class);
 
-            firstJob.setJarByClass(Map1.class);
+			firstJob.setInputFormatClass(SequenceFileInputFormat.class);
+			if (runJob2)
+				firstJob.setOutputFormatClass(SequenceFileOutputFormat.class);
 
-            /* You may need to change things here */
-            firstJob.setMapOutputKeyClass(Text.class);
-            firstJob.setMapOutputValueClass(Text.class);
-            firstJob.setOutputKeyClass(DoubleWritable.class);
-            firstJob.setOutputValueClass(Text.class);
-            /* End region where we expect you to perhaps need to change things. */
+			FileInputFormat.addInputPath(firstJob, inputPath);
+			FileOutputFormat.setOutputPath(firstJob, middleOut);
 
-            firstJob.setMapperClass(Map1.class);
-            firstJob.setReducerClass(Reduce1.class);
-            firstJob.setNumReduceTasks(reduceCount);
+			firstJob.waitForCompletion(true);
+		}
 
+		if (runJob2) {
+			Job secondJob = new Job(conf, "job2");
 
-            if(combiner)
-                firstJob.setCombinerClass(Combine1.class);
+			secondJob.setJarByClass(Map1.class);
+			/* You may need to change things here */
+			secondJob.setMapOutputKeyClass(DoubleWritable.class);
+			secondJob.setMapOutputValueClass(Text.class);
+			secondJob.setOutputKeyClass(DoubleWritable.class);
+			secondJob.setOutputValueClass(Text.class);
+			/* End region where we expect you to perhaps need to change things. */
 
-            firstJob.setInputFormatClass(SequenceFileInputFormat.class);
-            if(runJob2)
-                firstJob.setOutputFormatClass(SequenceFileOutputFormat.class);
+			secondJob.setMapperClass(Map2.class);
+			secondJob.setReducerClass(Reduce2.class);
 
-            FileInputFormat.addInputPath(firstJob, inputPath);
-            FileOutputFormat.setOutputPath(firstJob, middleOut);
+			secondJob.setInputFormatClass(SequenceFileInputFormat.class);
+			secondJob.setOutputFormatClass(TextOutputFormat.class);
+			secondJob.setNumReduceTasks(1);
 
-            firstJob.waitForCompletion(true);
-        }
+			FileInputFormat.addInputPath(secondJob, middleOut);
+			FileOutputFormat.setOutputPath(secondJob, finalOut);
 
-        if(runJob2) {
-            Job secondJob = new Job(conf, "job2");
-
-            secondJob.setJarByClass(Map1.class);
-            /* You may need to change things here */
-            secondJob.setMapOutputKeyClass(DoubleWritable.class);
-            secondJob.setMapOutputValueClass(Text.class);
-            secondJob.setOutputKeyClass(DoubleWritable.class);
-            secondJob.setOutputValueClass(Text.class);
-            /* End region where we expect you to perhaps need to change things. */
-
-            secondJob.setMapperClass(Map2.class);
-            secondJob.setReducerClass(Reduce2.class);
-
-            secondJob.setInputFormatClass(SequenceFileInputFormat.class);
-            secondJob.setOutputFormatClass(TextOutputFormat.class);
-            secondJob.setNumReduceTasks(1);
-
-
-            FileInputFormat.addInputPath(secondJob, middleOut);
-            FileOutputFormat.setOutputPath(secondJob, finalOut);
-
-            secondJob.waitForCompletion(true);
-        }
-    }
+			secondJob.waitForCompletion(true);
+		}
+	}
 
 }
+
